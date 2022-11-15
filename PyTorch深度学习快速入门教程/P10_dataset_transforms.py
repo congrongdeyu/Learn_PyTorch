ca55767eceb3f52873407f1e0d00d0201e87d2a7
DingYu <congrongdeyu@gmail.com>
2022-11-15 22:59:26 +0800

    update

---
+++ PyTorch深度学习快速入门教程/P10_dataset_transforms.py
@@ -0,0 +1,4 @@
+import torchvision
+
+train_set = torchvision.datasets.CIFAR10(root="./dataset", train=True, download=True)
+test_set = torchvision.datasets.CIFAR10(root="./dataset", train=False, download=True)

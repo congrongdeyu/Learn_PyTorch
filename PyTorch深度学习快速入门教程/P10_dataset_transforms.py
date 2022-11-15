4412436a1d3b82831351e0c5bb8143f4b5e1e415
DingYu <congrongdeyu@gmail.com>
2022-11-15 23:18:30 +0800

    update

--- PyTorch深度学习快速入门教程/P10_dataset_transforms.py
+++ PyTorch深度学习快速入门教程/P10_dataset_transforms.py
@@ -1,4 +1,18 @@
 import torchvision
 
+# 下载训练数据集和测试数据集
 train_set = torchvision.datasets.CIFAR10(root="./dataset", train=True, download=True)
 test_set = torchvision.datasets.CIFAR10(root="./dataset", train=False, download=True)
+
+# 查看测试数据集的元素
+print(test_set[0])
+
+# 查看测试数据集的分类
+print(test_set.classes)
+
+# 查看图片和标签
+img, target = test_set[0]
+print(img)
+print(target)
+print(test_set.classes[target])
+img.show()
